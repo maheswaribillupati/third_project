f5b41e11434a92bb7851c0232996bcc9155b9a78
billupati maheswari <bmaheswari474@gmail.com>
2023-03-09 22:03:49 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def rajesh(request):
+    return HttpResponse("<h1>HE IS MY BEST BROTHER</h1>")
+def rajesh(request):
+    return HttpResponse("<h1>I MISS YOU BROTHER</h1>")
---
+++ app2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def mahendhra(request):
+    return HttpResponse("<h1><marquee>ANNA YOU ARE MY INSPIRATION</marquee></h1>")
+def mahendhra(request):
+    return HttpResponse("<h1><marquee>I LEARN SO MANY LESSONS FROM YOU BROTHER</marquee></h1>")
